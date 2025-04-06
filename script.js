bc4e770eafdf1258a27eea28e65c65a3b9d431bc
Joanna Cepuch <asiacepuch1@gmail.com>
2025-04-06 20:16:37 +0200

    change page structure, add json input fille

--- script.js
+++ script.js
@@ -5,11 +5,7 @@ const filterCards =(e) =>{
     document.querySelector(".active").classList.remove("active");
     e.target.classList.add("active")
     cards.forEach(card => {
-        
         (card.dataset.name === e.target.dataset.name || e.target.dataset.name === "all")? card.classList.remove("hide") : card.classList.add("hide")
-        
-
-       
     })
 
 }
