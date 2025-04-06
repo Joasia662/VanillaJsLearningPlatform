099ee759cb3758aaf4010800bf5becaa351f6cd4
Joanna Cepuch <asiacepuch1@gmail.com>
2025-04-06 19:17:41 +0200

    create project with responsive course gallery

---
+++ script.js
@@ -0,0 +1,18 @@
+const filterButtons = document.querySelectorAll(".filters button");
+const cards = document.querySelectorAll(".cards .card");
+
+const filterCards =(e) =>{
+    document.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active")
+    cards.forEach(card => {
+        
+        (card.dataset.name === e.target.dataset.name || e.target.dataset.name === "all")? card.classList.remove("hide") : card.classList.add("hide")
+        
+
+       
+    })
+
+}
+
+
+filterButtons.forEach(button => button.addEventListener("click", filterCards))
